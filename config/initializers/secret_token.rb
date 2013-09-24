b40bd0e0de56e085b9c2de36121327658b8e93f4
courte <courteney.ervin@gmail.com>
2013-09-24 14:46:33 -0400

    Initial commit -- spanking new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassionPro::Application.config.secret_token = 'a674130d19b670a298481409dbb8557168b2889a4b7afce8ad9ec0e26d4e3b321109e98f04c254e94d2c920ef681e8c8e417f96daeed4a54798f8b84732532a5'
